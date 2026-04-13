0074be12b9e284833d4dfbece4e798ccb14a0b35
Khaled Alhossien <hossink131@gmail.com>
2026-04-13 15:23:22 +0300

    Fix Command class Style

--- Application/Features/Department/Command/Delete/DeleteDepartmentCommand.cs
+++ Application/Features/Department/Command/Delete/DeleteDepartmentCommand.cs
@@ -2,13 +2,5 @@
 
 namespace Application.Features.Department.Command.Delete
 {
-    public class DeleteDepartmentCommand : IRequest<bool> 
-    {
-        public int Id { get; }
-
-        public DeleteDepartmentCommand(int id)
-        {
-            Id = id;
-        }
-    }
+    public record DeleteDepartmentCommand(int Id) : IRequest<bool>;
 }
