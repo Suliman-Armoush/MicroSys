ad33c55d47334181910d2a241208a27a3eac0efd
Khaled Alhossien <hossink131@gmail.com>
2026-04-13 15:23:22 +0300

    Fix Command class Style

--- Application/Features/Department/Queries/GetById/GetDepartmentByIdQuery.cs
+++ Application/Features/Department/Queries/GetById/GetDepartmentByIdQuery.cs
@@ -3,13 +3,5 @@ using MediatR;
 
 namespace Application.Features.Department.Queries.GetById
 {
-    public class GetDepartmentByIdQuery : IRequest<DepartmentResponseDto>
-    {
-        public int Id { get; }
-
-        public GetDepartmentByIdQuery(int id)
-        {
-            Id = id;
-        }
-    }
+    public record GetDepartmentByIdQuery(int Id) : IRequest<DepartmentResponseDto>;
 }
