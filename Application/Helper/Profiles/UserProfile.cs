e54677d2bbb15489b657f67ba77344a4b6693eb3
Khaled Alhossien <hossink131@gmail.com>
2026-04-15 18:10:48 +0300

    Fix Arch Service Rigestration For All Layers

---
+++ Application/Helper/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Helper.Profiles
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+
+        }
+    }
+}
