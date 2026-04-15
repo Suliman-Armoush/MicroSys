90fbf10c4e57177c1cfa4157484009c890a83e9d
Khaled Alhossien <hossink131@gmail.com>
2026-04-15 18:10:48 +0300

    Fix Arch Service Rigestration For All Layers

---
+++ Application/Helper/Profiles/DepartmentProfile.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.Request;
+using Application.DTOs.Response;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Application.Helper.Profiles
+{
+    public class DepartmentProfile : Profile
+    {
+
+        public DepartmentProfile()
+        {
+          
+            CreateMap<Department, DepartmentResponseDto>().ReverseMap();
+            CreateMap<DepartmentRequestDto, Department>().ReverseMap();
+        }
+
+
+    }
+}
---
+++ Presentation/AddPresentationServices.cs
@@ -0,0 +1,26 @@
+﻿using Application;
+using Infrastructure;
+using System.Runtime.CompilerServices;
+
+namespace Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentationServices(this IServiceCollection services,  IConfiguration configuration)
+        {
+            services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            services.AddOpenApi();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+
+
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(configuration);
+
+
+            return services;
+        }
+    }
+}
