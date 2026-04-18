71e721701716438e08b1cb0e03230acbcc4a323c
Suliman Armoush <suliman221232@hotmail.com>
2026-04-18 22:44:23 +0300

    fix auth and token handling

--- Presentation/Controllers/RoleController.cs
+++ Presentation/Controllers/RoleController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "admin")]
     [ApiController]
     [Route("api/Role")]
     public class RoleController : ControllerBase
