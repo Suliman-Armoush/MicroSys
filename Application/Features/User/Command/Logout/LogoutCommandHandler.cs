0742673633623241c107195294cc65fa0888dac2
Homam Nasser <homam.naser.3@gmail.com>
2026-04-19 18:06:04 +0300

    Add LogOut

---
+++ Application/Features/User/Command/Logout/LogoutCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.User.Command.Logout
+{
+    public class LogoutCommandHandler : IRequestHandler<LogoutCommand, bool>
+    {
+        private readonly IJwtService _jwtService;
+
+        public LogoutCommandHandler(IJwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        public async Task<bool> Handle(LogoutCommand request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.Token) || !request.Token.StartsWith("Bearer "))
+            {
+                throw new ArgumentException("Invalid or missing Bearer token.");
+            }
+
+            var cleanToken = request.Token.Replace("Bearer ", "");
+
+            var result = await _jwtService.RevokeToken(cleanToken);
+
+            return result;
+        }
+    }
+}
