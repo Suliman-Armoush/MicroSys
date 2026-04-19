343e27044e5d1649d47f05dd39d8587f4f009fa8
Homam Nasser <homam.naser.3@gmail.com>
2026-04-19 18:06:04 +0300

    Add LogOut

---
+++ Application/Features/User/Command/Logout/LogoutCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.User.Command.Logout
+{
+    public record LogoutCommand(string Token) : IRequest<bool>;
+}
---
+++ Application/Features/User/Command/Logout/LogoutCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.User.Command.Logout
+{
+    public class LogoutCommandValidator : AbstractValidator<LogoutCommand>
+    {
+        public LogoutCommandValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty().WithMessage("Token is required for logout.");
+        }
+    }
+}
