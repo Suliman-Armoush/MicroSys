6d2d7ac8b98d73dcccb139c0608b1502c74587e9
Homam Nasser <homam.naser.3@gmail.com>
2026-04-28 17:47:36 +0300

    Edit MikrotikController and make Host,Report Controller

---
+++ Presentation/Controllers/HostController.cs
@@ -0,0 +1,55 @@
+﻿using Application.DTOs.Response;
+using Application.Features.Mikrotik.Command.DeleteAllFromHost;
+using Application.Features.Mikrotik.Command.DeleteFromHost;
+using Application.Features.Mikrotik.Queries.GetAllHosts;
+using Application.Features.Mikrotik.Queries.SearchInHost;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/Host")]
+    public class HostController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public HostController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<List<MikrotikHostResponse>>> GetHosts()
+        {
+            var hosts = await _mediator.Send(new GetAllMikrotikHostsQuery());
+            return Ok(hosts);
+        }
+
+        [HttpGet("Search")]
+        public async Task<ActionResult<List<MikrotikHostResponse>>> SearchHosts([FromQuery] string term)
+        {
+            var hosts = await _mediator.Send(new SearchMikrotikHostsQuery(term));
+            return Ok(hosts);
+        }
+
+        [HttpDelete("Delete/{macAddress}")]
+        public async Task<IActionResult> RemoveHost(string macAddress)
+        {
+            var result = await _mediator.Send(new RemoveMikrotikHostCommand(macAddress));
+
+            if (!result) return NotFound("Host not found or already disconnected.");
+
+            return Ok(new { Message = $"Host with MAC {macAddress} has been kicked successfully." });
+        }
+
+        [HttpDelete("Delete/All")]
+        public async Task<IActionResult> ClearAllHosts()
+        {
+            var result = await _mediator.Send(new RemoveAllMikrotikHostsCommand());
+            if (!result) return BadRequest("Could not clear hosts list.");
+
+            return Ok(new { Message = "All hosts have been disconnected successfully." });
+        }
+    }
+}
---
+++ Presentation/Controllers/ReportController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Features.Mikrotik.Queries.DepartmentConsumption;
+using Application.Features.Mikrotik.Queries.GetMyDepartmentConsumption;
+using Application.Features.Mikrotik.Queries.ReportDetailedConsumption;
+using Application.Features.Mikrotik.Queries.ReportTotalConsumption;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/Report")]
+    public class ReportController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("Export/Departments/Consumption")]
+        public async Task<IActionResult> ExportExcel()
+        {
+            var fileBytes = await _mediator.Send(new ExportMikrotikExcelQuery());
+
+            string fileName = $"Mikrotik_Consumption_{DateTime.Now:yyyyMMdd}.xlsx";
+
+            return File(
+                fileBytes,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                fileName
+            );
+        }
+
+        [HttpGet("Export/Detailed/Consumption")]
+        public async Task<IActionResult> ExportDetailedReport()
+        {
+            var fileBytes = await _mediator.Send(new ExportDetailedMikrotikReportQuery());
+            return File(
+                fileBytes,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Detailed_Mikrotik_Report.xlsx"
+            );
+        }
+
+        [HttpGet("Departments/Consumption")]
+        public async Task<IActionResult> GetDepartmentsConsumption()
+        {
+            var result = await _mediator.Send(new GetDepartmentsConsumptionQuery());
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("My/Department/Usage")]
+        public async Task<IActionResult> GetMyDepartmentUsage()
+        {
+            var deptIdClaim = User.FindFirst("DepartmentId")?.Value;
+
+            if (string.IsNullOrEmpty(deptIdClaim))
+                return Unauthorized("لم يتم العثور على معرف القسم في بيانات الدخول.");
+
+            var result = await _mediator.Send(new GetMyDepartmentConsumptionQuery(int.Parse(deptIdClaim)));
+
+            return Ok(result);
+        }
+    }
+}
