d079b8f00acaf829c367a6277c2aff12f24952ca
Homam Nasser <homam.naser.3@gmail.com>
2026-04-29 19:26:06 +0300

    Add Get User

---
+++ Application/Features/Mikrotik/Queries/GetUser/GetMikrotikUserByNameQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.Mikrotik.Queries.GetUser
+{
+    public record GetMikrotikUserByNameQuery(string Username) : IRequest<MikrotikUserInformationResponse>;
+}
