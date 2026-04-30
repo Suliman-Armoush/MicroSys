b70fad659792782ce849b574b2a38b63c282bdea
Khaled Alhossien <hossink131@gmail.com>
2026-04-30 21:47:24 +0300

    Add Department UI CRUDD

--- UI/Models/Response/DepartmentResponseDto.cs
+++ UI/Models/Response/DepartmentResponseDto.cs
@@ -1,19 +1,31 @@
 ﻿using System.Text.Json.Serialization;
+using UI.Models.Enums;
 
 namespace UI.Models.Response
 {
+    //public class DepartmentResponseDto
+    //{
+    //    [JsonPropertyName("id")]
+    //    public int Id { get; set; }
+
+    //    [JsonPropertyName("name")]
+    //    public string Name { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("type")]
+    //    public string Type { get; set; } = string.Empty;   // <- string, NOT int
+
+    //    [JsonPropertyName("dvrNum")]
+    //    public int DvrNum { get; set; }
+    //}
+
     public class DepartmentResponseDto
     {
-        [JsonPropertyName("id")]
         public int Id { get; set; }
-
-        [JsonPropertyName("name")]
         public string Name { get; set; } = string.Empty;
 
-        [JsonPropertyName("type")]
-        public string Type { get; set; } = string.Empty;   // <- string, NOT int
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public DepartmentTypes Type { get; set; }
 
-        [JsonPropertyName("dvrNum")]
         public int DvrNum { get; set; }
     }
 }
