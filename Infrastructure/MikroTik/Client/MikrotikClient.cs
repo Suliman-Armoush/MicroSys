026097cabfa0d07ed1f73e4bfffde5cd6b68c236
Suliman Armoush <suliman221232@hotmail.com>
2026-04-30 15:19:22 +0300

    clear massage with microtik connection exeption

--- Infrastructure/MikroTik/Client/MikrotikClient.cs
+++ Infrastructure/MikroTik/Client/MikrotikClient.cs
@@ -1,7 +1,6 @@
-﻿using Domain.Entities;
-using Infrastructure.Persistence;
-using Infrastructure.Persistence.Data;
+﻿using Infrastructure.Persistence.Data;
 using tik4net;
+using System.Net.Sockets;
 
 public class MikrotikClient
 {
@@ -17,19 +16,44 @@ public class MikrotikClient
     var sysInfo = _context.SysInfos.FirstOrDefault();
 
     if (sysInfo == null)
-      throw new Exception("SysInfo not found in database");
+      throw new Exception("MikroTik settings were not found in the database.");
 
-    if (string.IsNullOrEmpty(sysInfo.MikroTikIp))
-      throw new Exception("MikroTik IP is null");
+    if (string.IsNullOrWhiteSpace(sysInfo.MikroTikIp))
+      throw new Exception("MikroTik IP address is missing.");
 
-    var connection = ConnectionFactory.CreateConnection(TikConnectionType.Api);
+    if (string.IsNullOrWhiteSpace(sysInfo.Username))
+      throw new Exception("MikroTik username is missing.");
 
-    connection.Open(
-        sysInfo.MikroTikIp,
-        sysInfo.Username,
-        sysInfo.Password
-    );
+    if (string.IsNullOrWhiteSpace(sysInfo.Password))
+      throw new Exception("MikroTik password is missing.");
 
-    return connection;
+    try
+    {
+      var connection = ConnectionFactory.CreateConnection(TikConnectionType.Api);
+
+      connection.Open(
+          sysInfo.MikroTikIp,
+          sysInfo.Username,
+          sysInfo.Password
+      );
+
+      return connection;
+    }
+    catch (TikConnectionException)
+    {
+      throw new Exception("Failed to login to MikroTik: invalid username or password.");
+    }
+    catch (SocketException)
+    {
+      throw new Exception("Unable to connect to MikroTik: please check the IP address or network connection.");
+    }
+    catch (TimeoutException)
+    {
+      throw new Exception("Connection timeout: MikroTik device is not responding.");
+    }
+    catch (Exception ex)
+    {
+      throw new Exception($"An unexpected error occurred while connecting to MikroTik: {ex.Message}");
+    }
   }
 }
