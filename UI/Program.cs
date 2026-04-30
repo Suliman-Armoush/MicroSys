d40befc3365ab575fcf410c1256cd1a0e154f50d
Suliman Armoush <suliman221232@hotmail.com>
2026-04-30 18:36:06 +0300

    dont take it

--- UI/Program.cs
+++ UI/Program.cs
@@ -7,6 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // استدعاء طريقة التسجيل مع تمرير رابط الـ API
-builder.Services.AddUIServices("https://localhost:54982"); // ضع عنوان API الخاص بك
+builder.Services.AddUIServices("https://localhost:44345");
 
 await builder.Build().RunAsync();
