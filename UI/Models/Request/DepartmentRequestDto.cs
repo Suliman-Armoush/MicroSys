f85c25814be2f4eaf40e6ada3e40bda9840014bf
Khaled Alhossien <hossink131@gmail.com>
2026-04-30 21:47:24 +0300

    Add Department UI CRUDD

---
+++ UI/Models/Request/DepartmentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using UI.Models.Enums;
+
+namespace UI.Models.Request
+{
+    public class DepartmentRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public DepartmentTypes Type { get; set; }
+
+        public int DvrNum { get; set; }
+    }
+}
