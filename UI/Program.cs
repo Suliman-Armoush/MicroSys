acdda00dc7738a6ae3d9f11df0190868a1dee276
Suliman Armoush <suliman221232@hotmail.com>
2026-05-02 00:13:01 +0300

    UI sys info view

--- UI/Program.cs
+++ UI/Program.cs
@@ -7,6 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // استدعاء طريقة التسجيل مع تمرير رابط الـ API
-builder.Services.AddUIServices("https://localhost:44345");
+builder.Services.AddUIServices("https://localhost:44345"); // ضع عنوان API الخاص بك
 
 await builder.Build().RunAsync();
---
+++ UI/Services/Reposetories/SysInfosService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using UI.Models.Request;
+using UI.Models.Response;
+using UI.Services.Interfaces;
+
+namespace UI.Services.Reposetories
+{
+  public class SysInfosService : ISysInfosService
+  {
+    private readonly HttpClient _httpClient;
+
+    public SysInfosService(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+    public async Task<List<SysInfosResponseDto>> GetAsync()
+    {
+      return await _httpClient.GetFromJsonAsync<List<SysInfosResponseDto>>("api/SysInfo")
+             ?? new List<SysInfosResponseDto>();
+    }
+    public async Task UpdateAsync(int id, SysInfosRequestDto dto)
+    {
+      var response = await _httpClient.PutAsJsonAsync($"api/SysInfo/{id}", dto);
+      response.EnsureSuccessStatusCode();
+    }
+  }
+}
