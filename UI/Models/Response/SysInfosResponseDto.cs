0d867a24fbc47e5ed4e27996863e1c708fac73ef
Suliman Armoush <suliman221232@hotmail.com>
2026-05-02 00:13:01 +0300

    UI sys info view

---
+++ UI/Models/Response/SysInfosResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace UI.Models.Response
+{
+  public class SysInfosResponseDto
+  {
+    public int Id { get; set; }
+    public string MikroTikIp { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public double DvrPrice { get; set; }
+
+    public int segment1 { get; set; }
+    public int segment2 { get; set; }
+    public int segment3 { get; set; }
+    public int segment4 { get; set; }
+    public int segment5 { get; set; }
+    public int segment6 { get; set; }
+  }
+}
---
+++ UI/Services/Interfaces/ISysInfosService.cs
@@ -0,0 +1,11 @@
+﻿using UI.Models.Request;
+using UI.Models.Response;
+
+namespace UI.Services.Interfaces
+{
+  public interface ISysInfosService
+  {
+    Task<List<SysInfosResponseDto>> GetAsync();
+    Task UpdateAsync(int id, SysInfosRequestDto dto);
+  }
+}
