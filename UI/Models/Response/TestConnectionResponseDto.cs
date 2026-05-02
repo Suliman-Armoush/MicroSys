6c343fba421fd2c552ccd45682b73e0e550fc92d
Suliman Armoush <suliman221232@hotmail.com>
2026-05-02 11:52:22 +0300

    Activate button Test Connection

---
+++ UI/Models/Response/TestConnectionResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace UI.Models.Response
+{
+  public class TestConnectionResponseDto
+  {
+    public bool Success { get; set; }
+  }
+}
--- UI/Services/Interfaces/IMikrotikService.cs
+++ UI/Services/Interfaces/IMikrotikService.cs
@@ -3,32 +3,34 @@ using UI.Models.Response;
 
 namespace UI.Services.Interfaces
 {
-    public interface IMikrotikService
-    {
-        Task<List<MikrotikUserResponseDto>> GetAllUsersAsync();
-        Task<List<MikrotikProfileResponseDto>> GetAllProfilesAsync();
-        Task<List<MikrotikServerResponseDto>> GetServersListAsync();
-        Task<MikrotikUserInformationResponseDto?> CreateUserAsync(CreateMikrotikUserRequestDto user);
+  public interface IMikrotikService
+  {
+    Task<bool> TestConnectionAsync();
 
-        Task<MikrotikUserInformationResponseDto?> GetUserByNameAsync(string username);
-        Task<MikrotikUserInformationResponseDto?> UpdateUserAsync(string currentUsername, UpdateMikrotikUserRequestDto model);
+    Task<List<MikrotikUserResponseDto>> GetAllUsersAsync();
+    Task<List<MikrotikProfileResponseDto>> GetAllProfilesAsync();
+    Task<List<MikrotikServerResponseDto>> GetServersListAsync();
+    Task<MikrotikUserInformationResponseDto?> CreateUserAsync(CreateMikrotikUserRequestDto user);
 
-        Task<bool> DeleteUserAsync(string username);
+    Task<MikrotikUserInformationResponseDto?> GetUserByNameAsync(string username);
+    Task<MikrotikUserInformationResponseDto?> UpdateUserAsync(string currentUsername, UpdateMikrotikUserRequestDto model);
 
-        Task<string?> DisableUserAsync(string username);
-        Task<string?> EnableUserAsync(string username);
-        Task<List<MikrotikUserResponseDto>> SearchUsersAsync(string term);
+    Task<bool> DeleteUserAsync(string username);
+
+    Task<string?> DisableUserAsync(string username);
+    Task<string?> EnableUserAsync(string username);
+    Task<List<MikrotikUserResponseDto>> SearchUsersAsync(string term);
 
 
-        Task<List<MikrotikHostResponse>> GetAllHostsAsync();
-        Task<List<MikrotikHostResponse>> SearchHostsAsync(string term);
+    Task<List<MikrotikHostResponse>> GetAllHostsAsync();
+    Task<List<MikrotikHostResponse>> SearchHostsAsync(string term);
 
-        Task<bool> RemoveHostAsync(string macAddress);
+    Task<bool> RemoveHostAsync(string macAddress);
 
-        Task<bool> RemoveAllHostsAsync();
+    Task<bool> RemoveAllHostsAsync();
     Task<bool> ResetAllCountersAsync();
 
 
 
-    }
+  }
 }
