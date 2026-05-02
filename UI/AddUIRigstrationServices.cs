216efd438d56a47f24c8c73a624a88163e89f09e
Suliman Armoush <suliman221232@hotmail.com>
2026-05-02 12:41:47 +0300

    UI Add User CRUD view

--- UI/AddUIRigstrationServices.cs
+++ UI/AddUIRigstrationServices.cs
@@ -8,26 +8,27 @@ using UI.Services.Reposetories;
 namespace UI
 {
 
-    public static class UIServiceRegistration
+  public static class UIServiceRegistration
+  {
+    public static void AddUIServices(this IServiceCollection services, string apiBaseAddress)
     {
-        public static void AddUIServices(this IServiceCollection services, string apiBaseAddress)
-        {
-            // إعداد HttpClient الأساسي مع عنوان الـ API
-            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
+      // إعداد HttpClient الأساسي مع عنوان الـ API
+      services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
 
-            // تسجيل خدمات Blazor
-            services.AddBlazoredLocalStorage();
-            services.AddAuthorizationCore();
-            services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-            services.AddScoped<IAuthService, AuthService>();
-            services.AddScoped<IRoleService, RoleService>();
-            services.AddScoped<IMikrotikService, MikrotikService>();
-            services.AddScoped<IDepartmentService, DepartmentService>();
-            services.AddScoped<IReportService, ReportService>();
+      // تسجيل خدمات Blazor
+      services.AddBlazoredLocalStorage();
+      services.AddAuthorizationCore();
+      services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+      services.AddScoped<IAuthService, AuthService>();
+      services.AddScoped<IRoleService, RoleService>();
+      services.AddScoped<IMikrotikService, MikrotikService>();
+      services.AddScoped<IDepartmentService, DepartmentService>();
+      services.AddScoped<IReportService, ReportService>();
       services.AddScoped<ISysInfosService, SysInfosService>();
+      services.AddScoped<IUserService, UserService>();
 
       // يمكنك إضافة أي خدمات أخرى هنا مستقبلاً مثل IDepartmentService, IUserService إلخ.
     }
-    }
+  }
 
 }
---
+++ UI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using UI.Models.Request;
+using UI.Models.Response;
+
+namespace UI.Services.Interfaces
+{
+  public interface IUserService
+  {
+    Task<List<UserResponseDto>> GetAllAsync();
+    Task<UserResponseDto> GetByIdAsync(int id);
+    Task<UserResponseDto> CreateAsync(UserRequestDto dto);
+    Task<UserResponseDto> UpdateAsync(int id, UserRequestDto dto);
+    Task DeleteAsync(int id);
+  }
+}
