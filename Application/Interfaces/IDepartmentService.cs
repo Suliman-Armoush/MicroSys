f5d7f38227db96db95d5063bc61f2264d6edba52
Suliman Armoush <suliman221232@hotmail.com>
2026-06-07 16:57:49 +0300

    create user with create department

--- Application/Interfaces/IDepartmentService.cs
+++ Application/Interfaces/IDepartmentService.cs
@@ -1,24 +1,27 @@
-﻿using Domain.Entities;
+﻿using Application.DTOs.Request;
+using Application.DTOs.Response;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Application.DTOs.Request;
-using Application.DTOs.Response;
 namespace Application.Interfaces
 {
-    public interface IDepartmentService
-    {
-        Task<bool> AddAsync(Department department);
-        Task<bool> SaveChangesAsync();
+  public interface IDepartmentService
+  {
+    Task<Department> AddDep(Department department);
 
-        Task<Department> GetByIdAsync(int id);
+    Task<bool> AddAsync(Department department);
+    Task<bool> SaveChangesAsync();
 
-        Task<bool> UpdateAsync(Department department);
+    Task<Department> GetByIdAsync(int id);
 
-        Task<bool> DeleteAsync(Department department);
+    Task<bool> UpdateAsync(Department department);
 
-        Task<List<Department>> GetAllAsync();
-        Task<bool> HasUsersAsync(int departmentId);
-        Task<bool> ExistsAsync(int departmentId);
-    }
+    Task<bool> DeleteAsync(Department department);
+
+    Task<List<Department>> GetAllAsync();
+    Task<bool> HasUsersAsync(int departmentId);
+    Task<bool> ExistsAsync(int departmentId);
+  }
 }
--- Infrastructure/Persistence/Repositories/DepartmentService.cs
+++ Infrastructure/Persistence/Repositories/DepartmentService.cs
@@ -2,55 +2,66 @@
 using Domain.Entities;
 using Infrastructure.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence.Repositories
 {
-    public class DepartmentService : IDepartmentService
+  public class DepartmentService : IDepartmentService
+  {
+    private readonly DataContext _context;
+
+    public DepartmentService(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public DepartmentService(DataContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<bool> AddAsync(Department department)
-        {
-            await _context.Departments.AddAsync(department);
-            return await SaveChangesAsync();
-        }
-        public async Task<bool> SaveChangesAsync()
-        {
-            return (await _context.SaveChangesAsync()) > 0;
-        }
-        public async Task<Department> GetByIdAsync(int id)
-        {
-            return await _context.Departments.FindAsync(id);
-        }
-        public async Task<bool> UpdateAsync(Department department)
-        {
-            _context.Departments.Update(department);
-            return await SaveChangesAsync();
-        }
-
-        public async Task<bool> DeleteAsync(Department department)
-        {
-            _context.Departments.Remove(department);
-            return await SaveChangesAsync();
-        }
-        public async Task<List<Department>> GetAllAsync()
-        {
-            return await _context.Departments.ToListAsync();
-        }
-
-        public async Task<bool> HasUsersAsync(int departmentId)
-        {
-            return await _context.Users.AnyAsync(u => u.DepartmentId == departmentId);
-        }
-        public async Task<bool> ExistsAsync(int departmentId)
-        {
-            return await _context.Departments.AnyAsync(d => d.Id == departmentId);
-        }
+      _context = context;
     }
+
+    public async Task<bool> AddAsync(Department department)
+    {
+      await _context.Departments.AddAsync(department);
+      return await SaveChangesAsync();
+    }
+
+    public async Task<Department> AddDep(Department department)
+    {
+      var dep = await _context.Departments.AddAsync(department);
+      await SaveChangesAsync();
+
+      return dep.Entity;
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+      return (await _context.SaveChangesAsync()) > 0;
+    }
+    public async Task<Department> GetByIdAsync(int id)
+    {
+      return await _context.Departments.FindAsync(id);
+    }
+
+    public async Task<bool> UpdateAsync(Department department)
+    {
+      _context.Departments.Update(department);
+      return await SaveChangesAsync();
+    }
+
+    public async Task<bool> DeleteAsync(Department department)
+    {
+      _context.Departments.Remove(department);
+      return await SaveChangesAsync();
+    }
+    public async Task<List<Department>> GetAllAsync()
+    {
+      return await _context.Departments.ToListAsync();
+    }
+
+    public async Task<bool> HasUsersAsync(int departmentId)
+    {
+      return await _context.Users.AnyAsync(u => u.DepartmentId == departmentId);
+    }
+    public async Task<bool> ExistsAsync(int departmentId)
+    {
+      return await _context.Departments.AnyAsync(d => d.Id == departmentId);
+    }
+  }
 }
