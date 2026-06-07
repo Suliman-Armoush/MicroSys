df62c835e335367d46db83969334f4f5bb6b4a0e
Suliman Armoush <suliman221232@hotmail.com>
2026-06-07 16:57:49 +0300

    create user with create department

--- Application/Features/Department/Command/Create/CreateDepartmentCommandHandler.cs
+++ Application/Features/Department/Command/Create/CreateDepartmentCommandHandler.cs
@@ -1,31 +1,44 @@
-﻿using Application.DTOs.Response;
+﻿using Application.DTOs.Request;
+using Application.DTOs.Response;
 using Application.Interfaces;
 using AutoMapper;
-using Domain.Entities;
 using MediatR;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Application.Features.Department.Command.Create
 {
-    public class CreateDepartmentCommandHandler : IRequestHandler<CreateDepartmentCommand, DepartmentResponseDto>
+  public class CreateDepartmentCommandHandler : IRequestHandler<CreateDepartmentCommand, DepartmentResponseDto>
+  {
+    private readonly IDepartmentService _departmentService;
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public CreateDepartmentCommandHandler(IDepartmentService departmentService, IMapper mapper, IUserService userService)
     {
-        private readonly IDepartmentService _departmentService;
-        private readonly IMapper _mapper;
-
-        public CreateDepartmentCommandHandler(IDepartmentService departmentService, IMapper mapper)
-        {
-            _departmentService = departmentService;
-            _mapper = mapper;
-        }
-
-        public async Task<DepartmentResponseDto> Handle(CreateDepartmentCommand request, CancellationToken cancellationToken)
-        {
-            var department = _mapper.Map<Domain.Entities.Department>(request.DepartmentDto);
-
-            await _departmentService.AddAsync(department);
-
-            return _mapper.Map<DepartmentResponseDto>(department);
-        }
+      _departmentService = departmentService;
+      _userService = userService;
+      _mapper = mapper;
     }
+
+    public async Task<DepartmentResponseDto> Handle(CreateDepartmentCommand request, CancellationToken cancellationToken)
+    {
+      var departmentDto = _mapper.Map<Domain.Entities.Department>(request.DepartmentDto);
+      var department = await _departmentService.AddDep(departmentDto);
+
+      var userDto = new UserRequestDto
+      {
+        Name = department.Name + " manager",
+        UserName = department.Name,
+        Password = "12345678",
+        RoleId = 2,
+        DepartmentId = department.Id,
+      };
+
+      var user = _mapper.Map<Domain.Entities.User>(userDto);
+      user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+
+      await _userService.AddAsync(user);
+
+      return _mapper.Map<DepartmentResponseDto>(department);
+    }
+  }
 }
