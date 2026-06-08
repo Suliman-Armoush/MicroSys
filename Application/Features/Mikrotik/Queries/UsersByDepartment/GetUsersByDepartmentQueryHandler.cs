757c49c44eac490cf01fe9a6cdd9262de365cba2
Suliman Armoush <suliman221232@hotmail.com>
2026-06-08 15:37:33 +0300

    get users by department manager

---
+++ Application/Features/Mikrotik/Queries/UsersByDepartment/GetUsersByDepartmentQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.DTOs.Response;
+using Application.Features.Mikrotik.Queries.GetAllUser;
+using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.Mikrotik.Queries.UsersByDepartment
+{
+  internal class GetUsersByDepartmentQueryHandler
+
+    : IRequestHandler<GetUsersByDepartmentQuery, List<MikrotikUserResponse>>
+  {
+    private readonly IMikrotikService _mikrotik;
+
+    public GetUsersByDepartmentQueryHandler(IMikrotikService mikrotik)
+    {
+      _mikrotik = mikrotik;
+    }
+
+    public async Task<List<MikrotikUserResponse>> Handle(
+        GetUsersByDepartmentQuery request,
+        CancellationToken cancellationToken)
+    {
+      return await _mikrotik.GetUsersByDepartmentAsync();
+    }
+  }
+}
---
+++ Presentation/Controllers/Microtik/ManagerController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Mikrotik.Queries.GetAllUser;
+using Application.Features.Mikrotik.Queries.UsersByDepartment;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers.Microtik
+{
+  [ApiController]
+  [Route("api/Manager")]
+  public class ManagerController : ControllerBase
+  {
+    private readonly IMediator _mediator;
+
+    public ManagerController(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+
+    [Authorize(Roles = "Manager")]
+    [HttpGet("UsersByDepartment")]
+    public async Task<IActionResult> GetUsersByDepartment()
+    {
+      var result = await _mediator.Send(new GetUsersByDepartmentQuery());
+      return Ok(result);
+    }
+  }
+}
