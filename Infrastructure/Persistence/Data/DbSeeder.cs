72a085c762e634a8fd5906b92e977acaa072db73
Khaled Alhossien <hossink131@gmail.com>
2026-06-09 11:57:26 +0300

    Fux Admin Seed + Disabel - Enabel User

--- Infrastructure/Persistence/Data/DbSeeder.cs
+++ Infrastructure/Persistence/Data/DbSeeder.cs
@@ -3,48 +3,53 @@ using Domain.Entities;
 
 namespace Infrastructure.Persistence.Data
 {
-  public static class DbSeeder
-  {
-    public static async Task SeedAsync(DataContext context)
+    public static class DbSeeder
     {
-      if (!context.Roles.Any())
-      {
-        context.Roles.AddRange(
-            new Role { Name = "Admin" },
-            new Role { Name = "Manager" },
-            new Role { Name = "ACC" }
-        );
-
-        await context.SaveChangesAsync();
-      }
-
-      // Seed Admin User
-      if (!context.Users.Any(u => u.UserName == "Admin"))
-      {
-        var adminRole = context.Roles.First(r => r.Name == "Admin");
-
-        var admin = new User
+        public static async Task SeedAsync(DataContext context)
         {
-          Name = "Administrator",
-          UserName = "Admin",
-          PasswordHash = BCrypt.Net.BCrypt.HashPassword("it@123456"),
-          RoleId = adminRole.Id,
-        };
+            if (!context.Roles.Any())
+            {
+                context.Roles.AddRange(
+                    new Role { Name = "Admin" },
+                    new Role { Name = "Manager" },
+                    new Role { Name = "ACC" }
+                );
 
-        context.Users.Add(admin);
-        await context.SaveChangesAsync();
-      }
+                await context.SaveChangesAsync();
+            }
 
-      if (!context.SysInfos.Any())
-      {
-        context.SysInfos.AddRange(
-            new SysInfo { MikroTikIp = "10.10.10.88" , Username = "it", Password = "it@123456" , DvrPrice = 10000 }
-        );
+            // Seed Admin User
+            if (!context.Users.Any(u => u.UserName == "Admin"))
+            {
+                var adminRole = context.Roles.First(r => r.Name == "Admin");
 
-        await context.SaveChangesAsync();
-      }
+                var admin = new User
+                {
+                    Name = "Administrator",
+                    UserName = "Admin",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("it@123456"),
+                    RoleId = adminRole.Id,
+                    CreatePerm = true,
+                    UpdatePerm = true,
+                    ChangePerm = true
 
+
+                };
+
+                context.Users.Add(admin);
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.SysInfos.Any())
+            {
+                context.SysInfos.AddRange(
+                    new SysInfo { MikroTikIp = "10.10.10.88", Username = "it", Password = "it@123456", DvrPrice = 10000 }
+                );
+
+                await context.SaveChangesAsync();
+            }
+
+        }
     }
-  }
 
 }
